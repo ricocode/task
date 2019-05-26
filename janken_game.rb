b4b7641286a21331344a8d0be08c0d9b478185c1
Noriko Abe <code.norikoabe@gmail.com>
2019-05-26 13:06:13 +0000

    提出１

--- janken_game.rb
+++ janken_game.rb
@@ -3,27 +3,19 @@ class Player
   def initialize
     @hand = hand
   end
-
   def player_hand  #player手を選択
-    index = ["『じゃんけん...』,（数字を入力してください）","0: グー","1: チョキ","2: パー"]
+    index = ["『じゃんけん...』（数字を入力してください）","0: グー","1: チョキ","2: パー"]
     index.each do |content|
       puts content
     end
 
-    @hand = gets.to_i
-
-    if hand < 0 || hand > 2
-      puts "0,1,2以外できません。再入力してください"
-      return player_hand()
-    elsif hand == 0
-      puts "player;グー"
-      return 0
-    elsif hand == 1
-      puts "player;チョキ"
-      return 1
+    @hand = gets.chomp
+    if @hand == "0"|| @hand == "1" || @hand == "2"
+      hand = gets.to_i
+      return hand
     else
-      puts "player;パー"
-      return 2
+      puts "0,1,2以外できません。再入力してください"
+      Player.new.player_hand
     end
   end
 end
@@ -33,37 +25,40 @@ class Enemy
   def initialize
    @hand = hand
   end
-  
   def enemy_hand
     # グー、チョキ、パーの値をランダムに出力するメソッドの処理をこの中に作成する  
     @hand = rand(0..2)
-    if hand == 0
-      puts "enemy; グー"
-      return 0
-    elsif hand == 1
-      puts "enemy; チョキ"
-      return 1
-    else
-      puts "enemy; パー" 
-      return 2
-    end #if
+    return hand
   end #def
 end #class
 
-
 class Janken
   def pon(player_hand,enemy_hand) 
+    if player_hand == 0
+      puts "player;グー"
+    elsif player_hand == 1
+      puts "player;チョキ"
+    elsif hand == 2
+      puts "player;パー"
+    end
+
+    if enemy_hand == 0
+      puts "enemy;グー"
+    elsif enemy_hand == 1
+      puts "enemy;チョキ"
+    elsif enemy_hand == 2
+      puts "enemy;パー"
+    end
+
     result = (player_hand - enemy_hand + 3) % 3
     puts "（計算結果　#{result})"
     if result == 0
       puts "『あいこで』\n\n"  
       pon(Player.new.player_hand, Enemy.new.enemy_hand)
-      elsif  result == 2
+    elsif  result == 2
       puts "win! （あなたの勝ちです）"
-#     return 2
-      else #result == 1
+    else 
       puts "lose! （相手の勝ちです）"
-#     return 1
     end
   end
 end
